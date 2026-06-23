ce7c42c1250f83d139c4cc6ed90b25aa6993025d
Rutvik2302 <rutviktank23@gmail.com>
2026-06-23 08:44:45 +0530

    Remove unused consoles

--- src/app.js
+++ src/app.js
@@ -13,8 +13,7 @@ app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// Routes imports 
-console.log("run")
+
 
 const userRoute = require('./routes/user.route.js');
 
